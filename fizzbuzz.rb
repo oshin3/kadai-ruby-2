bd209fae3d5b33222cf98822db88af7d6783302e
shin <shin3.prg@gmail.com>
2021-06-08 07:12:09 +0000

    FizzBuzzの表示条件を冒頭に移動

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 15 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
-  elsif num % 15 == 0
-    return "FizzBuzz"
   else
     return num
   end
